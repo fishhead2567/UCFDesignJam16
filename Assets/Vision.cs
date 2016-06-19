c4dbb81c4507673df2b187b1f9fc09f4a435d433
Mohit <bmohit415@gmail.com>
2016-06-18 21:22:38 -0400

    Uncertain

---
+++ Assets/Vision.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vision : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter () {
+	
+		if (GameObject.FindGameObjectWithTag ("Crate")) {
+		 
+
+
+		}
+
+		if (GameObject.FindGameObjectWithTag ("FogTile")) {
+		
+		
+		}
+		if (GameObject.FindGameObjectWithTag ("Enemy")) {
+
+
+		}
+
+	
+	}
+}
